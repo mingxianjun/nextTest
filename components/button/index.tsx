1375e55e3f6c4a7adbeeb808dbf21c1e17dbcedd
明玄 <mingxianjun@163.com>
2022-03-03 20:16:52 +0800

    style:登录页面样式简单处理

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from './style.module.scss';
 
 type ButtonType = 'default' | 'primary' | 'disable' | 'warning';
 type ButtonHTMLType = 'submit' | 'button' | 'reset';
-type ButtonSize = 'default' | 'small' | 'middle' | 'large';
+type ButtonSize = '' | 'small' | 'middle' | 'large';
 
 export interface ButtonProps extends Pick<React.ButtonHTMLAttributes<any>, 'onClick' | 'title' | 'id'> {
   type?: ButtonType,
@@ -16,13 +17,18 @@ export const Button = ({
                          children,
                          htmlType = 'button',
                          type = 'default',
-                         size = 'default',
+                         size = '',
                          loading = false,
                          ...restProps
                        }: ButtonProps) => {
   return (
     <button type={htmlType}
-            className={type + ' ' + `size-${size}` + (loading ? ' loading' : '')}
+            className={
+              styles.ui + ' ' +
+              styles[type] +
+              (size ? ' ' + styles[size] : '') +
+              (loading ? ' ' + styles['loading'] : '')
+            }
             {...restProps}>
       {children || null}
     </button>
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,26 +1,67 @@
-import React, {useState} from "react";
+import React, {FormEvent, useState} from "react";
 import Head from 'next/head'
+import Link from 'next/link';
 import styles from '../styles/Login.module.scss';
 import {Button} from "../components";
 
 function Login() {
 
-  const [name, setName] = useState('');
+  const [tel, setTel] = useState<string>();
+  const [password, setPassword] = useState<string>();
+  const [loading, setLoading] = useState(false);
 
-  const handleClick = () => {
-    setName('yes')
-    console.log(name)
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    setLoading(true);
+    await setTimeout(() => {
+      console.log(tel, password);
+      // setLoading(false);
+    }, 2000);
+  }
+
+  const handleChangeTel = ({target}: React.ChangeEvent<HTMLInputElement>) => {
+    setTel(target.value);
+  }
+  const handleChangePassword = ({target}: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(target.value);
   }
 
   return (
     <div className={styles.wrap}>
       <Head>
-        <title>登录页面{name}</title>
-        <meta name="description" content="Learn forms with Next.js" />
-        <link rel="icon" href="/favicon.ico" />
+        <title>登录页面</title>
+        <meta name="description" content="Learn forms with Next.js"/>
+        <link rel="icon" href="/favicon.ico"/>
       </Head>
-      登录页面
-      <Button type={'primary'} htmlType={'submit'} onClick={handleClick}>登录</Button>
+      <h1>登录系统</h1>
+      <form onSubmit={handleSubmit}>
+        <div className={styles.formItem}>
+          <input required
+                 type='text'
+                 name='tel'
+                 onChange={handleChangeTel}
+                 maxLength={20}
+                 placeholder={'手机号'}/>
+        </div>
+        <div className={styles.formItem}>
+          <input required
+                 type='password'
+                 name='password'
+                 onChange={handleChangePassword}
+                 placeholder={'密码'}
+                 maxLength={20}/>
+        </div>
+        <div className={styles.formItem}>
+          <Button type={'primary'}
+                  loading={loading}
+                  size={'middle'}
+                  htmlType={'submit'}>登录</Button>
+        </div>
+        <div className={styles.formItem + ' ' + styles.between}>
+          <Link href={'/forget'}>忘记密码？</Link>
+          <Link href={'/register'}>没有账号，请注册</Link>
+        </div>
+      </form>
     </div>
   )
 }
